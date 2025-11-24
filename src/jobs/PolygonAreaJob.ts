0017750968ea38743dae239e88f51f6833abd272
Horacio Gouveia <gouveia.horacio@gmail.com>
2025-11-24 12:37:03 +0100

    chore: improveing code for tasks files

--- src/jobs/PolygonAreaJob.ts
+++ src/jobs/PolygonAreaJob.ts
@@ -4,30 +4,21 @@ import area from '@turf/area';
 import { Geometry, Polygon, MultiPolygon } from 'geojson';
 
 export class PolygonAreaJob implements Job {
-    async run(task: Task): Promise<{ output: string, error: boolean }> {
+    async run(task: Task): Promise<string> {
         console.log(`Running polygon area calculation for task ${task.taskId}...`);
-
         try {
             const inputGeometry: Geometry = JSON.parse(task.geoJson);
 
             if (inputGeometry.type !== 'Polygon' && inputGeometry.type !== 'MultiPolygon') {
-                return {
-                    output: `Invalid geometry type: ${inputGeometry.type}. Expected Polygon or MultiPolygon.`,
-                    error: true
-                };
+                throw new Error(`Invalid geometry type: ${inputGeometry.type}. Expected Polygon or MultiPolygon.`);
             }
 
             const polygonArea = area(inputGeometry as Polygon | MultiPolygon).toString();
             console.log(`The polygon area is ${polygonArea} square meters`);
-            return {
-                output: polygonArea,
-                error: false
-            };
+            return polygonArea
+
         } catch (error: any) {
-            return {
-                output: `Error running polygon area calculation for task ${task.taskId}: ${error.message}`,
-                error: true
-            };
+            throw new Error(`Error running polygon area calculation for task ${task.taskId}: ${error.message}`);
         }
     }
 }
--- src/jobs/ReportGenerationJob.ts
+++ src/jobs/ReportGenerationJob.ts
@@ -4,73 +4,88 @@ import { AppDataSource } from '../data-source';
 import { Result } from '../models/Result';
 import { TaskStatus } from '../workers/taskRunner';
 
-export class ReportGenerationJob implements Job {
-    async run(task: Task): Promise<{ output: string, error: boolean }> {
-        console.log(`Running report generation for task ${task.taskId}...`);
+interface TaskInfo {
+    taskId: string;
+    type: string;
+    output?: string;
+    error?: string;
+}
 
+interface Report {
+    workflowId: string;
+    tasks: TaskInfo[];
+    finalReport: string;
+}
+
+export class ReportGenerationJob implements Job {
+
+    /**
+     * Generates a report for a given workflowID and tasks.
+     * @param workflowId - The ID of the workflow.
+     * @param tasks - The tasks to generate the report for.
+     * @returns The report.
+     */
+    private async generateReport(workflowId: string, tasks: Task[]): Promise<Report> {
+        const resultRepository = AppDataSource.getRepository(Result);
+        let countOfTasksErrors = 0;
+        // Build the tasks array for the report
+        const tasksData = await Promise.all(
+            tasks.map(async (t) => {
+                const taskOutput = await resultRepository.findOne({
+                    where: { resultId: t.resultId }
+                });
+                if (t.status === TaskStatus.Failed) {
+                    countOfTasksErrors++;
+                }
+                const taskInfo: TaskInfo = {
+                    taskId: t.taskId,
+                    type: t.taskType,
+                }
+                if (t.status === TaskStatus.Completed) {
+                    taskInfo.output = taskOutput?.data || '';
+                } else {
+                    taskInfo.error = taskOutput?.data || '';
+                }
+
+                return taskInfo;
+            })
+        );
+
+        // Build the final report
+        return  {
+            workflowId: workflowId,
+            tasks: tasksData,
+            finalReport: `- Tasks completed: ${tasksData.length - countOfTasksErrors}. `+ 
+            `Tasks with errors: ${countOfTasksErrors}. `+ 
+            `Total tasks: ${tasksData.length}.`
+        };
+    }
+
+    async run(task: Task): Promise<Report> {
+        console.log(`Running report generation for task ${task.taskId}...`);
         const workflowId = task.workflow.workflowId;
         const taskRepository = AppDataSource.getRepository(Task);
-        const resultRepository = AppDataSource.getRepository(Result);
-
         // Load all tasks in the workflow
         const allTasks = await taskRepository.find({
             where: { workflow: { workflowId: workflowId } },
             relations: ['workflow'],
             order: { stepNumber: 'ASC' }
         });
-
         // Just get the tasks that are before the current task in the workflow
         const filteredTasks = allTasks.filter(t => t.stepNumber < task.stepNumber);
-
         // Ensure all preceding tasks are completed before proceeding
-        const incompleteTasks = filteredTasks.filter(t => t.status !== TaskStatus.Completed);
+        // Supposing that Failed tasks are also a completed task
+        const incompleteTasks = filteredTasks.filter(t => ![TaskStatus.Completed, TaskStatus.Failed].includes(t.status));
         if (incompleteTasks.length > 0) {
             const incompleteTaskIds = incompleteTasks.map(t => `${t.taskId} (step ${t.stepNumber}, status: ${t.status})`).join(', ');
             
-            return {
-                output: `Cannot generate report: ${incompleteTasks.length} preceding task(s) are not completed. ` +
+            throw new Error(`Cannot generate report: ${incompleteTasks.length} preceding task(s) are not completed. ` +
                 `Incomplete tasks: ${incompleteTaskIds}. ` +
-                `Report generation requires all preceding tasks to be completed.`,
-                error: true
-            };
+                `Report generation requires all preceding tasks to be completed.`);
         }
+        const report = await this.generateReport(workflowId, filteredTasks);
+        console.log(`Report for workflow ${workflowId} generated successfully. \n ${JSON.stringify(report, null, 2)}`);
 
-        let countOfTasksErrors = 0;
-
-        // Build the tasks array for the report
-        const tasksData = await Promise.all(
-            filteredTasks.map(async (t) => {
-
-                const taskOutput = await resultRepository.findOne({
-                    where: { resultId: t.resultId }
-                });
-
-                if (t.status === TaskStatus.Failed) {
-                    countOfTasksErrors++;
-                }
-
-                return {
-                    taskId: t.taskId,
-                    type: t.taskType,
-                    error: t.status === TaskStatus.Failed ? taskOutput?.data || '' : null,
-                    output: t.status === TaskStatus.Completed ? taskOutput?.data || '' : null,
-                };
-            })
-        );
-
-        // Build the final report
-        const report = JSON.stringify({
-            workflowId: workflowId,
-            tasks: tasksData,
-            finalReport: `Tasks completed: ${tasksData.length - countOfTasksErrors}. `+ 
-            `Tasks with errors: ${countOfTasksErrors}. `+ 
-            `Total tasks: ${tasksData.length}.`
-        });
-
-        console.log(`Report generated successfully for workflow ${workflowId}`);
-        return {
-            output: report,
-            error: false
-        };
+        return report;
     }
 }
